ec313e4d2e4e6ae3c2424d048b0b2bf5e5e71032
Valerio Ageno <valerio@sonicjobs.com>
2023-03-27 21:13:35 +0200

    chore: remove input comments

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,33 +1,3 @@
-// import type { InputEffect, Result } from './types'
-// import { InputHTMLAttributes, forwardRef, useImperativeHandle, useState } from 'react'
-// import React from 'react'
-// type InputProps = InputHTMLAttributes<HTMLInputElement>
-// const Input = forwardRef<InputEffect, InputProps>((props, ref): JSX.Element => {
-//   // Boolean with radio an checkbox
-//   const [state, setState] = useState<string | boolean>('')
-//   useImperativeHandle(
-//     ref,
-//     () => ({
-//       sendData(): Result {
-//         return { name: props?.name || 'former-input', value: state }
-//       },
-//     }),
-//     [props.name, state],
-//   )
-//   return (
-//     <input
-//       name="former-input"
-//       {...props}
-//       value={state as string}
-//       onChange={(e): void => {
-//         props.onChange?.(e)
-//         setState(e.target.value)
-//       }}
-//     />
-//   )
-// })
-// Input.displayName = 'FormerInput'
-// export default Input
 import withFormer, { WithFormerType } from './withFormer'
 import React, { InputHTMLAttributes } from 'react'
 
