5a08a7d08cac93178d52090cfa654fba1aaa75c2
Valerio Ageno <valerio@sonicjobs.com>
2023-05-14 11:01:35 +0200

    feat: test select reset

--- test/select.test.tsx
+++ test/select.test.tsx
@@ -1,6 +1,6 @@
-import { Select } from '../src'
+import { Form, Select, Value } from '../src'
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import * as React from 'react'
 
 describe('Select', () => {
@@ -15,5 +15,47 @@ describe('Select', () => {
     expect(screen.getByTestId('select')).toBeInTheDocument()
     expect(screen.getByTestId('select')).toBeInstanceOf(HTMLSelectElement)
     expect(screen.getByTestId('select')).toBeVisible()
+    expect(screen.queryByDisplayValue('2')).not.toBeInTheDocument()
+    fireEvent.change(screen.getByTestId('select'), { target: { value: '2' } })
+    expect(screen.queryByDisplayValue('2')).toBeInTheDocument()
+  })
+
+  it('Reset select', () => {
+    let payload: Record<string, Value> = {}
+    const onSubmitFn = (e: React.FormEvent<HTMLFormElement>, data: Record<string, Value>): void => {
+      e.preventDefault()
+      payload = data
+    }
+    render(
+      <Form onSubmit={onSubmitFn}>
+        <Select data-testid="select" initialValue="" name="select">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+        </Select>
+        <button type="button" name="reset" data-testid="reset">
+          Reset
+        </button>
+        <button type="submit" data-testid="submit">
+          Submit
+        </button>
+      </Form>,
+    )
+
+    fireEvent.click(screen.getByTestId('submit'))
+    expect(payload).toStrictEqual({ select: '' })
+
+    expect(screen.queryByDisplayValue('2')).not.toBeInTheDocument()
+    fireEvent.change(screen.getByTestId('select'), { target: { value: '2' } })
+    expect(screen.queryByDisplayValue('2')).toBeInTheDocument()
+
+    fireEvent.click(screen.getByTestId('submit'))
+    expect(payload).toStrictEqual({ select: '2' })
+
+    fireEvent.click(screen.getByTestId('reset'))
+    expect(screen.queryByDisplayValue('2')).not.toBeInTheDocument()
+
+    fireEvent.click(screen.getByTestId('submit'))
+    expect(payload).toStrictEqual({ select: '' })
   })
 })
